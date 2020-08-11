fc97967aae4f611539a7fa5ede1a21244f971bfa
Codehoff <enno.kirchhoff1911@gmail.com>
2020-08-11 14:47:50 +0200

    Lecture 96

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
         accentColor: Colors.amber,
+        fontFamily: "Quicksand",
+        textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+              fontFamily: "OpenSans",
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            )),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                  fontFamily: "OpenSans",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+        ),
       ),
       home: MyHomePage(),
     );
